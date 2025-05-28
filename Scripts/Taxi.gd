5a4e40a747f73819dc7793804975d08311751ae5
Jake Mitchell <j.mitchell7574@gmail.com>
2025-05-28 23:17:51 +0100

    implementing handbrake drift

--- Scripts/Taxi.gd
+++ Scripts/Taxi.gd
@@ -1,23 +1,54 @@
 class_name Taxi
 extends VehicleBody3D
 
-@export var MAX_STEER = 0.5
-@export var ENGINE_POWER = 500
-@export var MAX_SPEED = 30
+@export var BASE_STEER = 0.5
+@export var BASE_POWER = 500
+@export var BASE_TOP_SPEED = 30
+
+var steering_strength:
+	get:
+		if (Input.is_action_pressed("c_button") and not are_all_wheels_off()):
+			return BASE_STEER * 2
+		else:
+			return BASE_STEER
+
+
 var time_upside = 0
 
+var base_friction : float
+var brake_force : float = 0
+
+@onready var front_left: VehicleWheel3D = $FrontLeft
+@onready var back_left: VehicleWheel3D = $BackLeft
+@onready var front_right: VehicleWheel3D = $FrontRight
+@onready var back_right: VehicleWheel3D = $BackRight
+
+
+func _ready() -> void:
+	base_friction = front_left.wheel_friction_slip
+
 func _physics_process(delta: float) -> void:
-	steering = Input.get_axis("move_right", "move_left") * MAX_STEER
-	engine_force = Input.get_axis("c_button", "a_button") * ENGINE_POWER
+	steering = Input.get_axis("move_right", "move_left") * steering_strength
+	engine_force = Input.get_axis("b_button", "a_button") * BASE_POWER
 	
-	if (Input.is_action_pressed("b_button") and not are_all_wheels_off()):
-		var decay_factor = pow(0.5, delta / 2)
-		
-		# Apply the decay to the current speed
-		linear_velocity *= decay_factor
+	if (Input.is_action_pressed("c_button") and not are_all_wheels_off()):
+			back_left.brake = 500
+			back_right.brake = 500
+			back_left.wheel_friction_slip = base_friction / 5
+			back_right.wheel_friction_slip = base_friction / 5
+			front_left.wheel_friction_slip = base_friction / 5
+			front_right.wheel_friction_slip = base_friction / 5
+	else:
+		back_left.brake = 0
+		back_right.brake = 0
+		back_left.wheel_friction_slip = base_friction
+		back_right.wheel_friction_slip = base_friction
+		front_left.wheel_friction_slip = base_friction
+		front_right.wheel_friction_slip = base_friction
+			
 	
-	if linear_velocity.length() > MAX_SPEED:
-		linear_velocity = linear_velocity.normalized() * MAX_SPEED
+	if linear_velocity.length() > BASE_TOP_SPEED:
+		linear_velocity = linear_velocity.normalized() * BASE_TOP_SPEED
 	
 	if Input.is_action_just_pressed("reset_pos"):
 		position = Vector3(0, 5, 0)
@@ -35,6 +66,10 @@ func _physics_process(delta: float) -> void:
 	if global_position.y < -8:
 		position = Vector3(0, 5, 0)
 		rotation_degrees = Vector3(0, 0, 0)
+	
+	if Input.is_action_pressed(&"a_button") and Input.is_action_pressed(&"b_button"):
+		rotation_degrees.y += Input.get_axis("move_right", "move_left") * steering_strength
+		
 
 func are_all_wheels_off():
 	for child in get_children():
